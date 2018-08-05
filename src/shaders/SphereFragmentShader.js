3047bdb62c7e5e369bbbd03e87cd01b25461de30
Hamish Williams <tr4nsfuse@gmail.com>
2018-08-05 21:18:03 +1000

    Update three.js

--- src/shaders/SphereFragmentShader.js
+++ src/shaders/SphereFragmentShader.js
@@ -27,7 +27,7 @@ varying float noise;
 #include <fog_pars_fragment>
 #include <bsdfs>
 #include <lights_pars_begin>
-#include <lights_pars_maps>
+#include <envmap_physical_pars_fragment>
 #include <lights_phong_pars_fragment>
 #include <shadowmap_pars_fragment>
 #include <bumpmap_pars_fragment>
