6675ffa719b1ce342f15404564ead2512b849215
Hamish Williams <tr4nsfuse@gmail.com>
2018-11-22 21:33:59 +1100

    Initial cra2 support

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { hydrate, render } from 'react-dom';
 import App from './app/App';
-import registerServiceWorker from './registerServiceWorker';
+import * as serviceWorker from './serviceWorker';
 
 const rootElement = document.getElementById('root');
 if (rootElement.hasChildNodes()) {
@@ -10,4 +10,4 @@ if (rootElement.hasChildNodes()) {
   render(<App />, rootElement);
 }
 
-registerServiceWorker();
+serviceWorker.register();
