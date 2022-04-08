7db2f3c927c08deb67f6d3748c7edb41b1498da3
Hamish Williams <tr4nsfuse@gmail.com>
2022-04-09 08:43:34 +1000

    Update fragment shader

--- src/pages/Home/sphereFragShader.js
+++ src/pages/Home/sphereFragShader.js
@@ -1,3 +1,5 @@
+// An extension of three's default phong shader
+
 export default `
 #define PHONG
 
@@ -6,6 +8,7 @@ uniform vec3 emissive;
 uniform vec3 specular;
 uniform float shininess;
 uniform float opacity;
+
 uniform float time;
 varying vec2 vUv;
 varying vec3 newPosition;
@@ -13,20 +16,23 @@ varying float noise;
 
 #include <common>
 #include <packing>
+#include <dithering_pars_fragment>
 #include <color_pars_fragment>
 #include <uv_pars_fragment>
 #include <uv2_pars_fragment>
 #include <map_pars_fragment>
 #include <alphamap_pars_fragment>
+#include <alphatest_pars_fragment>
 #include <aomap_pars_fragment>
 #include <lightmap_pars_fragment>
 #include <emissivemap_pars_fragment>
+#include <envmap_common_pars_fragment>
 #include <envmap_pars_fragment>
-#include <gradientmap_pars_fragment>
+#include <cube_uv_reflection_fragment>
 #include <fog_pars_fragment>
 #include <bsdfs>
 #include <lights_pars_begin>
-#include <envmap_physical_pars_fragment>
+#include <normal_pars_fragment>
 #include <lights_phong_pars_fragment>
 #include <shadowmap_pars_fragment>
 #include <bumpmap_pars_fragment>
@@ -36,7 +42,7 @@ varying float noise;
 #include <clipping_planes_pars_fragment>
 
 void main() {
-  #include <clipping_planes_fragment>
+	#include <clipping_planes_fragment>
 
   vec3 color = vec3(vUv * (0.2 - 2.0 * noise), 1.0);
   vec3 finalColors = vec3(color.b * 1.5, color.r, color.r);
@@ -44,28 +50,30 @@ void main() {
   ReflectedLight reflectedLight = ReflectedLight(vec3(0.0), vec3(0.0), vec3(0.0), vec3(0.0));
   vec3 totalEmissiveRadiance = emissive;
 
-  #include <logdepthbuf_fragment>
-  #include <map_fragment>
-  #include <color_fragment>
-  #include <alphamap_fragment>
-  #include <alphatest_fragment>
-  #include <specularmap_fragment>
-  #include <normal_fragment_begin>
-  #include <normal_fragment_maps>
-  #include <emissivemap_fragment>
-  #include <lights_phong_fragment>
-  #include <lights_fragment_begin>
-  #include <lights_fragment_maps>
-  #include <lights_fragment_end>
-  #include <aomap_fragment>
+	#include <logdepthbuf_fragment>
+	#include <map_fragment>
+	#include <color_fragment>
+	#include <alphamap_fragment>
+	#include <alphatest_fragment>
+	#include <specularmap_fragment>
+	#include <normal_fragment_begin>
+	#include <normal_fragment_maps>
+	#include <emissivemap_fragment>
+	#include <lights_phong_fragment>
+	#include <lights_fragment_begin>
+	#include <lights_fragment_maps>
+	#include <lights_fragment_end>
+	#include <aomap_fragment>
 
-  vec3 outgoingLight = reflectedLight.directDiffuse + reflectedLight.indirectDiffuse + reflectedLight.directSpecular + reflectedLight.indirectSpecular + totalEmissiveRadiance;
-
-  #include <envmap_fragment>
-  #include <premultiplied_alpha_fragment>
-  #include <tonemapping_fragment>
-  #include <encodings_fragment>
-  #include <fog_fragment>
+	vec3 outgoingLight = reflectedLight.directDiffuse + reflectedLight.indirectDiffuse + reflectedLight.directSpecular + reflectedLight.indirectSpecular + totalEmissiveRadiance;
+  
+	#include <envmap_fragment>
+	#include <output_fragment>
+	#include <tonemapping_fragment>
+	#include <encodings_fragment>
+	#include <fog_fragment>
+	#include <premultiplied_alpha_fragment>
+	#include <dithering_fragment>
 
   gl_FragColor = vec4(outgoingLight, diffuseColor.a);
 }
