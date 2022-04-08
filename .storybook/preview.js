916fc6e2fb7a73772f41b1464a80d029531427c3
Hamish Williams <tr4nsfuse@gmail.com>
2022-04-09 09:11:16 +1000

    Reindex

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,8 +1,9 @@
+import '../src/app/reset.css';
+import '../src/app/App.css';
+import './preview.css';
+
 import { useEffect } from 'react';
 import ThemeProvider from '../src/components/ThemeProvider';
-import '../src/app/reset.css';
-import '../src/app/index.css';
-import './preview.css';
 
 export const decorators = [
   (Story, context) => {
