bc620035e0a66cddd718edecbb3b1962178a359a
Hamish Williams <tr4nsfuse@gmail.com>
2022-04-18 14:37:59 +1000

    Fix Firefox srcset resolution

--- src/utils/image.js
+++ src/utils/image.js
@@ -54,25 +54,21 @@ export function srcSetToString(srcSet = []) {
  */
 export async function generateImage(width = 1, height = 1) {
   return new Promise(resolve => {
-    try {
-      const canvas = document.createElement('canvas');
-      const ctx = canvas.getContext('2d');
+    const canvas = document.createElement('canvas');
+    const ctx = canvas.getContext('2d');
 
-      canvas.width = width;
-      canvas.height = height;
+    canvas.width = width;
+    canvas.height = height;
 
-      ctx.fillStyle = 'rgba(0, 0, 0, 0)';
-      ctx.fillRect(0, 0, width, height);
+    ctx.fillStyle = 'rgba(0, 0, 0, 0)';
+    ctx.fillRect(0, 0, width, height);
 
-      canvas.toBlob(async blob => {
-        if (!blob) throw new Error('Video thumbnail failed to load');
-        const image = URL.createObjectURL(blob);
-        canvas.remove();
-        resolve(image);
-      });
-    } catch (error) {
-      throw new Error(error);
-    }
+    canvas.toBlob(async blob => {
+      if (!blob) throw new Error('Video thumbnail failed to load');
+      const image = URL.createObjectURL(blob);
+      canvas.remove();
+      resolve(image);
+    });
   });
 }
 
@@ -92,17 +88,8 @@ export async function resolveSrcFromSrcSet({ srcSet, sizes }) {
   );
 
   const fakeSrcSet = sources.map(({ image, width }) => `${image} ${width}`).join(', ');
-  const fakeSrc = await loadImageFromSrcSet({
-    src: sources[0].src,
-    srcSet: fakeSrcSet,
-    sizes,
-  });
+  const fakeSrc = await loadImageFromSrcSet({ srcSet: fakeSrcSet, sizes });
 
-  // Clean up image urls to prevent memory leaks
-  for (const source of sources) {
-    URL.revokeObjectURL(source.image);
-  }
-
-  const videoSrc = sources.find(src => src.image === fakeSrc);
-  return videoSrc.src;
+  const output = sources.find(src => src.image === fakeSrc);
+  return output.src;
 }
