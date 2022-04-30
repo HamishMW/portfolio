aa7f45f1d12e451b9ae0eddcf839786bfef23c3b
Hamish Williams <tr4nsfuse@gmail.com>
2022-04-30 20:58:00 +1000

    Fix borked fonts

--- src/components/ThemeProvider/ThemeProvider.js
+++ src/components/ThemeProvider/ThemeProvider.js
@@ -1,8 +1,5 @@
-import GothamBoldItalic from 'assets/fonts/gotham-bold-italic.woff2';
 import GothamBold from 'assets/fonts/gotham-bold.woff2';
-import GothamBookItalic from 'assets/fonts/gotham-book-italic.woff2';
 import GothamBook from 'assets/fonts/gotham-book.woff2';
-import GothamMediumItalic from 'assets/fonts/gotham-medium-italic.woff2';
 import GothamMedium from 'assets/fonts/gotham-medium.woff2';
 import Head from 'next/head';
 import { createContext, useEffect } from 'react';
@@ -22,7 +19,7 @@ export const fontStyles = squish(`
   @font-face {
     font-family: "Gotham";
     font-weight: 400;
-    src: url(${GothamBookItalic}) format("woff2");
+    src: url(${GothamBook}) format("woff2");
     font-display: block;
     font-style: italic;
   }
@@ -38,7 +35,7 @@ export const fontStyles = squish(`
   @font-face {
     font-family: "Gotham";
     font-weight: 500;
-    src: url(${GothamMediumItalic}) format("woff2");
+    src: url(${GothamMedium}) format("woff2");
     font-display: block;
     font-style: italic;
   }
@@ -54,7 +51,7 @@ export const fontStyles = squish(`
   @font-face {
     font-family: 'Gotham';
     font-weight: 700;
-    src: url(${GothamBoldItalic}) format('woff2');
+    src: url(${GothamBold}) format('woff2');
     font-display: block;
     font-style: italic;
   }
