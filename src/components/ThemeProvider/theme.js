adc4db201775a11459d33825a856a1bad8e002b0
Tomás Palma <tomas@tomasps.com>
2024-06-26 18:39:40 +0200

    Oled dark mode
    Added true dark mode (0 0 0)

--- src/components/ThemeProvider/theme.js
+++ src/components/ThemeProvider/theme.js
@@ -109,7 +109,7 @@ const tokensMobileSmall = {
 // Tokens that change based on theme
 const dark = {
   themeId: 'dark',
-  rgbBackground: '17 17 17',
+  rgbBackground: '0 0 0',
   rgbBackgroundLight: '26 26 26',
   rgbPrimary: '0 229 255',
   rgbAccent: '0 229 255',
